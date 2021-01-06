55e63d3a49f53a2c11488dedc076ca10ca9220ca
tenhove.jasper <tenhove.jasper@gmail.com>
2021-01-06 20:42:10 +0100

    rps multiple args one message
    small fix

--- commands/rps.js
+++ commands/rps.js
@@ -25,28 +25,40 @@ module.exports = {
         }
 
         function withArgs(args) { //function for when there are arguments
+            var num_wins = 0;
+            var num_lost = 0;
             for (var i = 0; i < args.length; i++){
                 let options = ["rock", "paper", "scissors"]; //gets a random answer
                 let answer = options[Math.floor(Math.random() * options.length)];
+                var win = 0;
 
                 if (args[i] == answer) { //all available options for rock paper scissors
-                    userDraw(answer);
-                } else if (args[i] == "rock" && answer == "paper") {
-                    userLost(answer);
-                } else if (args[i] == "rock" && answer == "scissors") {
-                    userWin(answer);
-                } else if (args[i] == "paper" && answer == "scissors") {
-                    userLost(answer);
-                } else if (args[i] == "paper" && answer == "rock") {
-                    userWin(answer);
-                } else if (args[i] == "scissors" && answer == "rock") {
-                    userLost(answer);
-                } else if (args[i] == "scissors" && answer == "paper") {
-                    userWin(answer);
-                } else if (args[i] == "score") {
+                    win = 0;
+                } else if ((args[i] == "rock" && answer == "paper") || (args[i] == "paper" && answer == "scissors") || (args[i] == "scissors" && answer == "rock")) {
+                    win = 1;
+                    num_lost++;
+                } else if ((args[i] == "rock" && answer == "scissors") || (args[i] == "paper" && answer == "rock") || (args[i] == "scissors" && answer == "paper")) {
+                    win = 2;
+                    num_wins++;
+                }  else if (args[i] == "score") {
                     userScore(); //shows score
                 } else {
                     noArgs(); //sends game information because argument is invalid
+                }
+                if (args.length > 1){
+                    if ((i + 1) == args.length){
+                        userWin(answer, num_wins);
+                        userLost(answer, num_lost);
+                    }
+                } else {
+                    switch(win){
+                        case 0:
+                            userDraw(answer);
+                        case 1:
+                            userLost(answer, num_lost);
+                        case 2:
+                            userWin(answer, num_wins);
+                    }
                 }
             }
         }
@@ -56,19 +68,19 @@ module.exports = {
             setTimeout(() => message.channel.send("Let\'s try again!"), 800);
         }
 
-        function userWin(answer) { //gets called when the user wins
+        function userWin(answer, num_wins) { //gets called when the user wins
             message.channel.send(answer);
             setTimeout(() => message.channel.send("Congrats, you won!"), 800);
-            addWin();
+            addWin(num_wins);
         }
 
-        function userLost(answer) { //gets called when a user loses
+        function userLost(answer, num_lost) { //gets called when a user loses
             message.channel.send(answer);
             setTimeout(() => message.channel.send("Better luck next time!"), 800);
-            addLost();
+            addLost(num_lost);
         }
 
-        function addWin() { //adds a win to userdata
+        function addWin(num_wins) { //adds a win to userdata
             let user = message.author.tag.toString();
 
             if (!score[message.author.id]) score[message.author.id] = { //checks if user is in datasheet
@@ -77,10 +89,10 @@ module.exports = {
                 rps_lost: 0
             };
             let userData = score[message.author.id]; //adds a win
-            userData.rps_win++;
+            userData.rps_win += num_wins;
         }
 
-        function addLost() { //adds lost to userdata
+        function addLost(num_lost) { //adds lost to userdata
             let user = message.author.tag.toString();
 
             if (!score[message.author.id]) score[message.author.id] = {
@@ -89,7 +101,7 @@ module.exports = {
                 rps_lost: 0
             };
             let userData = score[message.author.id];
-            userData.rps_lost++;
+            userData.rps_lost += num_lost;
 
         }
 
